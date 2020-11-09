15898c2755156617b6abf61a6a4dbae1810a2db2
portgasdax99 <ngdaaz@gmail.com>
2020-11-09 11:36:35 +0700

    Create a map with black and white for chessboard

---
+++ app.js
@@ -0,0 +1,42 @@
+const board = document.querySelector(".board");
+const boardletters = document.querySelector(".letters")
+const boardnumbers = document.querySelector(".numbers")
+let letters = ["a","b","c","d","e","f","g","h"];
+let index = 0;
+let black = false;
+let num =1;
+
+// Create name of column and name of row in chessboard
+
+for(let i = 0; i < 8; ++i){
+    let letter = document.createElement("li");
+    letter.textContent = letters[i];    
+    boardletters.appendChild(letter);
+    let numbers = document.createElement("li");
+    numbers.textContent = num++;
+    boardnumbers.appendChild(numbers);
+}
+
+// Create each square for board and apply css property for each square
+
+for(let i = 0; i < 64; ++i){
+    const square = document.createElement("div");
+    if(black){
+        square.classList.add("square");
+         square.classList.add("black");
+         index++;
+         black = !black;
+    } else{
+        square.classList.add("square");
+        square.classList.add("white");
+        index++;
+        black = !black;
+    }
+
+    board.appendChild(square);
+
+    if(index === 8){
+        black = !black;
+        index = 0;
+    }
+}
