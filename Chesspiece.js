581adfcb6c86cbc837faef1cd85adb907447f62e
portgasdax99 <ngdaaz@gmail.com>
2020-11-12 08:52:53 +0700

    change the way create the chess table

---
+++ Chesspiece.js
@@ -0,0 +1,169 @@
+
+
+const chess = [
+    {
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['A','7']
+    },
+    {
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['B','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['C','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['D','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['E','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['F','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['G','7']
+    },{
+        player: 'white',
+        pieceType:'pawn',
+        image:'whitePawnImageObject',
+        currentSquare:['H','7']
+    },blackRock = {
+        player:'white',
+        pieceType:'rock',
+        image:'blackRockImageObject',
+        currentSquare:['H','8']
+    },whiteRock = {
+        player:'white',
+        pieceType:'rock',
+        image:'whiteRockImageObject',
+        currentSquare:['A','8']
+    },whiteKnight = {
+        player:'black',
+        pieceType:'knight',
+        image:'whiteKnightImageObject',
+        currentSquare:['G','8']
+    }, whiteKnight = {
+        player:'black',
+        pieceType:'knight',
+        image:'whiteKnightImageObject',
+        currentSquare:['B','8']
+    },whiteBishop = {
+        player:'black',
+        pieceType:'bishop',
+        image:'whiteBishopImageObject',
+        currentSquare:['F','8']
+    }, whiteBishop = {
+        player:'white',
+        pieceType:'bishop',
+        image:'whiteBishopImageObject',
+        currentSquare:['C','8']
+    },whiteQueen = {
+        player:'white',
+        pieceType:'queen',
+        image:'whiteQueenImageObject',
+        currentSquare:['D','8'],
+    }, whiteKing = {
+        player:'white',
+        pieceType:'king',
+        image:'whiteKingImageObject',
+        currentSquare:['E','8'],
+    },
+    //black
+        blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['H','2']
+    },  blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['G','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['F','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['E','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['D','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['C','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['B','2']
+    },blackPawn = {
+        player: 'black',
+        pieceType:'pawn',
+        image:'blackPawnImageObject',
+        currentSquare:['A','2']
+    },blackRock = {
+        player:'black',
+        pieceType:'rock',
+        image:'blackRockImageObject',
+        currentSquare:['H','1']
+    },blackRock = {
+        player:'black',
+        pieceType:'rock',
+        image:'blackRockImageObject',
+        currentSquare:['A','1']
+    },blackKnight = {
+        player:'black',
+        pieceType:'knight',
+        image:'blackKnightImageObject',
+        currentSquare:['G','1']
+    },blackKnight = {
+        player:'black',
+        pieceType:'knight',
+        image:'blackKnightImageObject',
+        currentSquare:['B','1']
+    },blackBishop = {
+        player:'black',
+        pieceType:'bishop',
+        image:'blackBishopImageObject',
+        currentSquare:['F','1']
+    },blackBishop = {
+        player:'black',
+        pieceType:'bishop',
+        image:'blackBishopImageObject',
+        currentSquare:['C','1']
+    }, blackQueen = {
+        player:'black',
+        pieceType:'queen',
+        image:'blackQueenImageObject',
+        currentSquare:['D','1'],
+    },blackKing = {
+        player:'black',
+        pieceType:'king',
+        image:'blackBishopImageObject',
+        currentSquare:['E','1'],
+    }
+    
+]
--- app.js
+++ app.js
@@ -1,170 +1,28 @@
-const board = document.querySelector(".board");
+
+const board = document.getElementById("Chessboard");
 const boardletters = document.querySelector(".letters")
 const boardnumbers = document.querySelector(".numbers")
-let letters = ["a","b","c","d","e","f","g","h"];
+let letters = ["A","B","C","D","E","F","G","H"];
 let index = 0;
 let black = false;
 let num =1;
 
-// Create name of column and name of row in chessboard
 
-for(let i = 0; i < 8; ++i){
-    let letter = document.createElement("li");
-    letter.textContent = letters[i];    
-    boardletters.appendChild(letter);
-    let numbers = document.createElement("li");
-    numbers.textContent = num++;
-    boardnumbers.appendChild(numbers);
-}
-
-const map = [
-    3, 5, 4, 2, 1, 4, 5, 3,
-    6, 6, 6, 6, 6, 6, 6, 6,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,   
-    12,12,12,12,12,12,12,12,
-    9,11,10, 8, 7,10,11, 9,
-  ];
-
-// Create each square for board and apply css property for each square, add chess pieces
-
-for(let i = 0; i < 64; ++i){
-    const square = document.createElement("div");
-    if(black){
-         square.classList.add("square");
-         square.classList.add("black");
-         index++;
-         black = !black;
-    } else{
-        square.classList.add("square");
-        square.classList.add("white");
-        index++;
-        black = !black;
+for(let i = 0; i < 8;++i){
+    for(let j = 0; j < 8; ++j){
+        const square = document.createElement("div");
+        board.appendChild(square);
+        square.classList.add(`square`);
+        square.classList.add(`${i % 2 ? 'odd' : 'even'}`)
+        square.id = `["${letters[j]}","${i + 1}"]`
     }
-
-    square.classList.add ("&#"+ (9811+map[i]));
-
-    if(index === 8){
-        black = !black;
-        index = 0;
-    }
-//Using Chess symbols in Unicode to create image of chess pieces
-    square.innerHTML = !map[i] ? "" : "&#"+ (9811+map[i]) +";";
-
-    // if(map[i] != ""){
-        // square.classList.add("&#" + (9811+map[i]) +";")
-    // }    
-//
-
-    board.appendChild(square);
-
-}
-
-
-
-
-//Chess pieces
-var chessPieces = {
-    'white': {
-         'king': '&#9812;',
-         'queen': '&#9813;',
-         'rook': '&#9814;',
-         'bishop': '&#9815;',
-         'knight': '&#9816;',
-         'pawn': '&#9817;'
-    },
-    'black': {
-         'king': '&#9818;',
-         'queen': '&#9819;',
-         'rook': '&#9820;',
-         'bishop': '&#9821;',
-         'knight': '&#9822;',
-         'pawn': '&#9823;'
-    }
-};  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}   
+
+chess.map((c,i) => {
+    const location = JSON.stringify(c.currentSquare)
+    const element = document.getElementById(location)
+    element.style.backgroundImage = `url(./merida/${c.image}.svg)`
+})
 
 
 
